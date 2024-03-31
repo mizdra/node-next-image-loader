f7adea71e2f65815cc00f2f062b2a2e8d9e2d24d
mizdra <pp.mizdra@gmail.com>
2024-03-31 17:21:46 +0900

    format

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vitest/config';
 import GithubActionsReporter from 'vitest-github-actions-reporter';
+import { defineConfig } from 'vitest/config';
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   test: {
+    // biome-ignore lint/complexity/useLiteralKeys: for compatibility with tsc
     reporters: process.env['GITHUB_ACTIONS'] ? ['default', new GithubActionsReporter()] : 'default',
     cache: {
       dir: 'node_modules/.cache/vitest',
