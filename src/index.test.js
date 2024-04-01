8ac02938e19b826c42a8f06e8830cb1aebc7e388
mizdra <pp.mizdra@gmail.com>
2024-04-02 00:29:28 +0900

    support Node.js v18

--- src/index.test.js
+++ src/index.test.js
@@ -1,13 +1,17 @@
 import { deepEqual } from 'node:assert/strict';
 import { readFile } from 'node:fs/promises';
 import bunAllGIF from '../assets/burnallgifs.png';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /**
  * @param {string} path
  * @returns {Promise<string>}
  */
 async function readAssetAsBase64(path) {
-  return await readFile(new URL(path, import.meta.url).pathname, 'base64');
+  return await readFile(join(__dirname, path), 'base64');
 }
 
 deepEqual(bunAllGIF, {
