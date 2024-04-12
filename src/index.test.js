80451048c511c937698cd1d5a809af9c6d58af62
mizdra <pp.mizdra@gmail.com>
2024-04-12 18:40:33 +0900

    add test cases for require

--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,8 @@ import { deepEqual } from 'node:assert/strict';
 import { readFile } from 'node:fs/promises';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { createRequire } from 'node:module';
+import test from 'node:test';
 import burnAllGIF from '../assets/burnallgifs.png';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -14,8 +16,20 @@ async function readAssetAsBase64(path) {
   return await readFile(join(__dirname, path), 'base64');
 }
 
-deepEqual(burnAllGIF, {
-  src: `data:image/png;base64,${await readAssetAsBase64('../assets/burnallgifs.png')}`,
-  width: 199,
-  height: 117,
+test('import', async () => {
+  deepEqual(burnAllGIF, {
+    src: `data:image/png;base64,${await readAssetAsBase64('../assets/burnallgifs.png')}`,
+    width: 199,
+    height: 117,
+  });
+});
+
+test('require', async () => {
+  const require = createRequire(import.meta.url);
+  const burnAllGIF = require('../assets/burnallgifs.png');
+  deepEqual(burnAllGIF, {
+    src: `data:image/png;base64,${await readAssetAsBase64('../assets/burnallgifs.png')}`,
+    width: 199,
+    height: 117,
+  });
 });
